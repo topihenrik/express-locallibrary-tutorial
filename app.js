f310fc84939f2d7ddb3758a1401228caec513687
topihenrik <trashtunnus@gmail.com>
2022-08-11 00:22:21 +0300

    Heroku deployment preparation

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(helmet());
 
 
 var mongoose = require("mongoose");
-var mongoDB = "mongodb+srv://admin:salis123@cluster0.5sypk.mongodb.net/?retryWrites=true&w=majority";
+var dev_db_url = "mongodb+srv://admin:salis123@cluster0.5sypk.mongodb.net/?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
